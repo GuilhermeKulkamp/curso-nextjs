ef9515a4ab199f828899bd6369fb8ca2a0a31c83
GuilhermeKulkamp <guilhermekulkamp@gmail.com>
2025-12-08 16:25:11 -0300

    Add: adicionada funcionalidade de formatar mensagem para whatsapp MOK

--- utils/geramensagem.js
+++ utils/geramensagem.js
@@ -1,13 +1,12 @@
 /**
  * Gera mensagem personalizada para Whatsapp
- * @module utils/contacts
+ * @module utils/geramensagem
  * @param {string} nome - Nome do contato
  * @param {string} servico - Serviço oferecido
  * @returns {string} Mensagem formatada
  */
 const gerarMensagemWhatsApp = (nome, servico) => {
-  const mensagem =
-    "Olá, ${nome}! Sou gerente de projetos e gostaria de discutir ${servico}. Podemos agendar?";
+  const mensagem = `Olá, ${nome}! Sou gerente de projetos e gostaria de discutir ${servico}. Podemos agendar?`;
   return mensagem;
 };
 
