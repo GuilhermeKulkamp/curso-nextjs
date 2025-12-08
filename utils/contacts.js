9e5b240c7be4355439d29dac7ce00a7b19c1e603
GuilhermeKulkamp <guilhermekulkamp@gmail.com>
2025-12-07 23:48:47 -0300

    feat: add function to generate personalized WhatsApp messages

---
+++ utils/contacts.js
@@ -0,0 +1,16 @@
+/**
+ * Gera mensagem personalizada para Whatsapp
+ * @module utils/contacts
+ * @param {string} nome - Nome do contato
+ * @param {string} servico - Serviço oferecido
+ * @returns {string} Mensagem formatada
+ */
+const gerarMensagemWhatsApp = (nome, servico) => {
+  const mensagem =
+    "Olá, ${nome}! Sou gerente de projetos e gostaria de discutir ${servico}. Podemos agendar?";
+  return mensagem;
+};
+
+// Uso
+console.log(gerarMensagemWhatsApp("João ninguém", "desenvolvimento web"));
+// Saída: "Olá, João ninguém! Sou gerente de projetos e gostaria de discutir desenvolvimento web. Podemos agendar?"
